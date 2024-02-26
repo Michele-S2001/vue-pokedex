e78ad55b2559a61b277dc12b8889d562bed7ded5
MicheleSerafini <serafini.michele01@gmail.com>
2024-02-26 15:15:24 +0100

    ADD > Initial setUp with a simple header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="page-header">
+    <div class="container">
+      <img class="poke-logo" src="../../img/Pokédex_logo.png" alt="pokedex logo">
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/vars' as *;
+.page-header {
+  padding: 20px 10px;
+  box-shadow: 0px 4px 10px 1px rgba($pokeBlue, $alpha: 0.7);
+  .poke-logo {
+    margin: 0 auto;
+    width: 240px;
+  }
+}
+
+</style>
